e1fe66ec80e9b674de7e8a80b0a5f6c72036e781
sabrina1405 <sabrina.s.souzarosa@gmail.com>
2024-10-16 19:59:59 -0300

    pagina 4 com css, html e java realizadas
    - Arrumar o Menu da pagina 4; ele não está abrindo;
    - Colocar o texto em colunas;

---
+++ javascript/pag4.js
@@ -0,0 +1,18 @@
+var selecionado = true
+
+function exibir() {
+    var barra = document.getElementById("aside");
+
+    if (selecionado == true) {
+        selecionado = false;
+        barra.style.display = 'block';
+        barra.style.transition = "1s";
+
+
+    } else if (selecionado == false){
+        selecionado = true;
+        barra.style.display = 'none';
+        barra.style.transition = "1s";
+    }
+    return;
+}
